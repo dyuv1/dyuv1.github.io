06f11649121782b0ef2405c14178176fa6db8817
Arghyadeep Dash <arghyadeepdash@Arghyadeeps-MacBook-Air.local>
2023-09-23 18:12:10 +0530

    open in new tab

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const express = require('express')
+import express from "express"
+import open from "open"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
 
 const app = express()
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
+const file = fileURLToPath(import.meta.url)
+const __dirname = dirname(file)
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
 })
 
-app.post('/word', (req, res) => {
-    const word = req.body.word.trim().toLowerCase()
-    res.redirect("http://wisdomlib.org/definition/"+word+"#sanskrit")
+app.post("/word", async (req, res) => {
+  const word = req.body.word.trim().toLowerCase()
+  await open("http://wisdomlib.org/definition/" + word + "#sanskrit")
 })
 
 const listener = app.listen(3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
+  console.log("Your app is listening on port " + listener.address().port)
 })
